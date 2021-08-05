7f5cdc3e27ff5c1d18c9c61ff5a72b7cbd3b0828
Kakeru <stand.by.we@gmail.com>
2021-08-05 22:33:05 +0900

    add

--- Sources/TestRepo/TestRepo.swift
+++ Sources/TestRepo/TestRepo.swift
@@ -1,3 +1,5 @@
-struct TestRepo {
-    var text = "Hello, World!"
+public struct TestRepo {
+    public var text = "Hello, World!"
+    internal var aaa = "i am internal"
+    var bbb = "i am bbb"
 }
